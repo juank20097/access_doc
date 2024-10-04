b1cb47ac424fc9b4a4e8976b959e7223b309985a
martinezjairo-d1 <martinezjairo.d1@gmail.com>
2024-10-04 15:23:16 -0500

    Initial configuration

---
+++ src/main/java/com/project/accessDoc/AccessDocApplication.java
@@ -0,0 +1,13 @@
+package com.project.accessDoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessDocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/accessDoc/AccessDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.accessDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
